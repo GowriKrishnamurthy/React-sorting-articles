3c047870a47091efb7bd3a2ee46ff29eca7c8141
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2021-04-23 22:05:21 +1000

    Added new articles component

---
+++ src/components/Articles.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Articles = ({ articles }) => {
+  return (
+    <div className="card w-50 mx-auto">
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Upvotes</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {articles.map((article, index) => {
+            return (
+              <tr data-testid="article" key={index}>
+                <td data-testid="article-title">{article.title}</td>
+                <td data-testid="article-upvotes">{article.upvotes}</td>
+                <td data-testid="article-date">{article.date}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Articles;
